f8faaa79879f863c17e222d2c4a2c9b0a438c6bd
zakachkin <67714481+zakachkin@users.noreply.github.com>
2026-05-30 12:35:54 +0300

    Add Linux TCP MSS clamp for proxy listener

---
+++ internal/utils/net_listener_linux.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"fmt"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+const proxyListenerMSS = 256
+
+func setProxyListenerSocketOptions(network, address string, conn syscall.RawConn) error {
+	var sockErr error
+
+	if err := conn.Control(func(fd uintptr) {
+		sockErr = unix.SetsockoptInt(
+			int(fd),
+			unix.IPPROTO_TCP,
+			unix.TCP_MAXSEG,
+			proxyListenerMSS,
+		)
+	}); err != nil {
+		return err //nolint: wrapcheck
+	}
+
+	if sockErr != nil {
+		return fmt.Errorf("set TCP_MAXSEG=%d: %w", proxyListenerMSS, sockErr)
+	}
+
+	return nil
+}
