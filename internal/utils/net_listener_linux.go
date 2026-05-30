22ec6d063958de7f53b808ffdb141570c2dd79c9
zakachkin <67714481+zakachkin@users.noreply.github.com>
2026-05-30 13:06:22 +0300

    Tune proxy listener MSS to 1000

--- internal/utils/net_listener_linux.go
+++ internal/utils/net_listener_linux.go
@@ -7,7 +7,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-const proxyListenerMSS = 1372
+const proxyListenerMSS = 1000
 
 func setProxyListenerSocketOptions(network, address string, conn syscall.RawConn) error {
 	var sockErr error
